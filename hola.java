4a2d75a6fe6df7c20b2366145a5a91ddbd34c30d
sentis <david.sentis@insbaixcamp.cat>
2013-02-26 17:21:50 +0100

    Add a new bucle to contain the projects.

--- hola.java
+++ hola.java
@@ -2,6 +2,8 @@ class hola{
 	//Això és un comentari
 	public static void main(String[] args){
 		//Aquesta linea treu per pantalla la següent frase:
-		System.out.println("El meu projecte");
+		for ( i = 0; i < 10; i++) {
+			System.out.println("El meu projecte " + i);
+		}
 	}
 }
