acb3f0514ce921e5a292d53a1cec8eaa4d4b7529
drakgoku12 <javier.gonzalez@insbaixcamp.cat>
2013-02-26 17:22:12 +0100

    This is my project that I change one line

--- hola.java
+++ hola.java
@@ -3,5 +3,6 @@ class hola{
 	public static void main(String[] args){
 		//Aquesta linea treu per pantalla la següent frase:
 		System.out.println("El meu projecte");
+		System.out.println("Aquest torna a ser una altra modificació");
 	}
 }
